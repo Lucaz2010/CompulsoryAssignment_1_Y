1a1e39aea468d16230bc28a293aa47a7b36bb5ba
davide-jorge <finndahumane3@protonmail.com>
2026-08-16 16:58:08 +0200

    1st milestone complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,23 @@
-import "./index.css"
-import logo from "./Y_ChatterLogo.png"
-import {useEffect, useState} from "react";
+import {createBrowserRouter, RouterProvider} from "react-router";
+import {FeedPage} from "@/pages/FeedPage.tsx";
+import {PostPage} from "@/pages/PostPage.tsx";
+import {CreatePostPage} from "@/pages/CreatePostPage.tsx";
 
-export function App() {
+const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <FeedPage/>,
+    },
+  {
+    path: "/posts/:id",
+    element: <PostPage/>,
+  },
+  {
+    path: "/create",
+    element: <CreatePostPage/>,
+  },
+]);
 
-  const [post, setPost] = useState()
-
-  useEffect(() => {
-
-    fetch('https://dummyjson.com/posts/').then(response => {
-      response.json().then(json => {
-        setPost(json)
-      })
-    })
-  }, []);
-
-  return (
-      <main className="min-h-screen bg-base-200 flex items-center justify-center p-8">
-        <div className="card bg-base-100 w-full max-w-md shadow-xl">
-          <div className="card-body">
-            <h1 className="card-title text-3xl">
-              Y Chatter
-            </h1>
-
-            <p>
-              Tailwind CSS and daisyUI are working!
-            </p>
-
-            <div className="card-actions justify-end">
-              <button className="btn btn-primary">
-                Awesome!
-              </button>
-            </div>
-          </div>
-        </div>
-      </main>
-      /*
-      <div>
-        <header>
-          <img src={logo} className="logo" width="240" />
-        </header>
-        {
-          JSON.stringify(post)
-        }
-      </div>
-       */
-  );
+export default function App() {
+  return <RouterProvider router={router} />
 }
-
-export default App;
